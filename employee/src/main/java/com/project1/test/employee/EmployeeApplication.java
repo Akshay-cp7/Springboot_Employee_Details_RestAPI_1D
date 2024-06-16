479fb208a581213e36d09942f01a07347122d77f
Akshay-cp7 <com.akshaycp@gmail.com>
2024-06-16 12:23:04 +0530

    first test

---
+++ employee/src/main/java/com/project1/test/employee/EmployeeApplication.java
@@ -0,0 +1,14 @@
+package com.project1.test.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+	public static void main(String[] args) {
+		//this is first test
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+}
---
+++ employee/src/main/java/com/project1/test/employee/model/CloudVendor.java
@@ -0,0 +1,6 @@
+package com.project1.test.employee.model;
+
+public class CloudVendor {
+
+
+}
